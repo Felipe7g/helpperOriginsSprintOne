77246b59acacb884dd5c770a26bf3b6856c17a6a
Matheus <felipematheuseliassantos@gmail.com>
2021-11-04 07:38:45 -0300

    feature: exercicio_1 feito

---
+++ exercicio_1/contador.js
@@ -0,0 +1,30 @@
+var inputVowels = document.getElementById("inputChars");
+var numberVowels = document.getElementById("showCountVowels");
+var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+var countVowels = 0;
+var fixedChar = inputVowels.value;
+function reset() {
+    inputVowels.value = '';
+    numberVowels.innerText = '';
+    fixedChar = '';
+    countVowels = 0;
+}
+//conta as vogais do texto pré-escrito.(referente a questão 1.a)
+function getFixedChar(str) {
+    for (var _i = 0, str_1 = str; _i < str_1.length; _i++) {
+        var char = str_1[_i];
+        if (vowels.includes(char)) {
+            countVowels++;
+        }
+        numberVowels.innerHTML = countVowels.toString();
+    }
+}
+getFixedChar(fixedChar);
+//quando o usuário digita algo no input.(referente a questão 1.b)
+inputVowels.addEventListener('keypress', function (event) { return countChars(event.key); });
+function countChars(char) {
+    if (vowels.includes(char)) {
+        countVowels++;
+        numberVowels.innerHTML = countVowels.toString();
+    }
+}
---
+++ exercicio_1/contador.ts
@@ -0,0 +1,32 @@
+const inputVowels = document.getElementById("inputChars") as HTMLInputElement;
+const numberVowels = document.getElementById("showCountVowels") as HTMLSpanElement;
+const vowels = ['a','e','i','o','u', 'A','E','I','O','U'];
+let countVowels: number = 0;
+let fixedChar = inputVowels.value;
+
+function reset() {
+  inputVowels.value = '';
+  numberVowels.innerText = '';
+  fixedChar = '';
+  countVowels= 0;
+}
+
+//conta as vogais do texto pré-escrito.(referente a questão 1.a)
+function getFixedChar(str: string) {
+  for (let char of str){
+     if (vowels.includes(char)){
+      countVowels++;
+     }
+     numberVowels.innerHTML = countVowels.toString();
+}}
+getFixedChar(fixedChar)
+
+//quando o usuário digita algo no input.(referente a questão 1.b)
+inputVowels.addEventListener('keypress', (event: KeyboardEvent) => countChars(event.key));
+
+function countChars(char: string) {
+  if(vowels.includes(char)) {
+    countVowels++
+    numberVowels.innerHTML = countVowels.toString();
+  }
+}
