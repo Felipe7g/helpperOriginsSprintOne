f055a888aaf9e574989d334d8d6095c635d6f680
Matheus <felipematheuseliassantos@gmail.com>
2021-11-09 21:23:07 -0300

    fix: correcoes exercicio2

--- exercicio_2/listagem.ts
+++ exercicio_2/listagem.ts
@@ -1,4 +1,11 @@
-let list = [
+interface Person
+{
+    id : number;
+    name : string;
+    bio : string;
+}
+
+let list: Array<Person> = [
   {
     id: 1,
     name: "Ada Lovelace",
@@ -23,7 +30,7 @@ let list = [
 
 //1.a
 function getBioImperativo(id: number): string {
-    for(let i=0; i < list.length; i++ ) {
+    for(let i=0; i < list.length; i++) {
         if(list[i].id === id) {
             console.log(list[i].bio,'=>bio imperativo');
             return list[i].bio;
@@ -46,9 +53,9 @@ function getNameImperativo(id: number): string {
 getNameImperativo(1);
 
 //1.c
-function deleteItemImperativo(id: number){
+function deleteItemImperativo(id: number): void {
   for(let i=0; i < list.length; i++ ) {
-    let delItem = list[i].id;
+    let delItem: number = list[i].id;
     if(delItem === id) {
         delItem-=1;
         const itemDeleted = list.splice(delItem, 1);
@@ -61,9 +68,9 @@ function deleteItemImperativo(id: number){
 deleteItemImperativo(4);
 
 //1.d
-function updateItemImperativo(id: number, type: string, value: string) {
+function updateItemImperativo(id: number, type: string, value: string): void {
   for(let i=0; i < list.length; i++ ) {
-    let update = list[i].id;
+    let update: number = list[i].id;
     if(update === id) {
         update-=1;
         if (type === "name") {
@@ -83,48 +90,41 @@ updateItemImperativo(3, "name", "Any");
 // parte funcional
 
 //a) Crie uma função que retorne a bio do id passado
-function getBio(id: number) {
-  const person = list.find((item) => item.id === id);
-  console.log(person?.bio);
+function getBio(list: Array<Person>, id: number): string{
+  return list.find((item: Person) => item.id === id)?.bio || 'Pessoa não encontrada';
 }
-getBio(1);
+console.log(getBio(list, 1));
 
 //b) Crie uma função que retorne a name do id passado
-function getName(id: number) {
-    const person = list.find((item) => item.id === id);
-    console.log(person?.name);
+function getName(list: Array<Person>, id: number): string {
+  return list.find((item: Person) => item.id === id)?.name || 'Pessoa não encontrada';
 }
-getName(1);
+console.log('Get name => ', getName(list, 1));
 
 // c) Crie uma função que apague um item da lista a partir de um id passado
-function deleteItem(id: number) {
-  const delItem = list.find((item) => item.id === id);
-  if (delItem) {
-    const index = list.indexOf(delItem);
-    const itemDeleted = list.splice(index, 1);
-    console.log("Item deletado:", itemDeleted);
-  } else {
-    console.log("Id não encontrado");
-  }
+function deleteItem(list: Array<Person>, id: number): Array<Person>  {
+  return list.filter((item) => item.id !== id);
 }
-deleteItem(2);
+console.log('Delete item: Array atualizado => ', deleteItem(list, 1));
 
 // d) Crie uma função que altere a bio ou o name a partir de um id passado
-function updateItem(id: number, type: string, value: string) {
+function updateItem(list: Array<Person>, id: number, type: string, value: string): Array<Person> | string {
   const update = list.find((item) => item.id === id);
   if (update) {
-    const index = list.indexOf(update);
+    const index: number = list.indexOf(update);
+    const newList: Array<Person> = list;
     if (type === "name") {
-      list[index].name = value;
+      newList[index].name = value;
     }
-
     if (type === "bio") {
-      list[index].bio = value;
+      newList[index].bio = value;
     }
+    return newList
   } else {
     console.log("id inexistente");
+    return "id inexistente";
   }
 }
-updateItem(1, "name", "felps");
+console.log(updateItem(list, 1, "name", "felps222"));
 
 console.log(list);
